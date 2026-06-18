9b44f41fb80c4bc95e36c016aa2c3243f48c0c77
Madhulatha17 <bandimadhulatha4@gmail.com>
2026-06-18 17:43:21 +0530

    Create SimpleStorage.sol

---
+++ smart_contract/SimpleStorage.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.20;
+
+contract SimpleStorage {
+
+    string public data;
+
+    function set(string memory _data) public {
+        data = _data;
+    }
+
+    function get() public view returns (string memory) {
+        return data;
+    }
+}
